3e5886adb4f03c37407bb6bf2eadf90fd380efe5
GanzStulle <42404708+GanzStulle@users.noreply.github.com>
2020-05-04 21:51:33 +0200

    update login/out
    add login and logout system

--- website/users/views.py
+++ website/users/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
-#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm #for your own user registration field
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        #verifie all input fields
-        if form.is_valid():
+        if form.is_valid(): #verifie all input fields
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'{username}, you account has been created. You are able to log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
